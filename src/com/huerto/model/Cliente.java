8a171946dfd3b33f29178269f7b9625e1c526e05
Sandra López Iglesias <slopeziglesias@danielcastelao.org>
2023-05-26 23:40:33 +0200

    base de las clases que usaremos en el proyecto

--- src/com/huerto/model/Cliente.java
+++ src/com/huerto/model/Cliente.java
@@ -1,4 +1,34 @@
 package com.huerto.model;
 
 public class Cliente {
+    // Atributos cliente
+    private Integer idCliente;
+    private String nombre;
+
+    // Constructores cliente
+    public Cliente(){
+
+    }
+
+    public Cliente(Integer idCliente, String nombre){
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+    }
+
+    // Getters y setters cliente
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }

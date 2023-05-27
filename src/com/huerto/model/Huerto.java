b40b132241c76fdbc8a102d64e9dc0ea7add1714
Sandra López <113535029+SanLopezIgle@users.noreply.github.com>
2023-05-27 11:29:27 +0200

    base de las clases que usaremos en el proyecto (#2)
    haciendo merge squash

--- src/com/huerto/model/Huerto.java
+++ src/com/huerto/model/Huerto.java
@@ -1,4 +1,34 @@
 package com.huerto.model;
 
 public class Huerto {
+    // Atributos huerto
+    private Integer idHuerto;
+    private String estado;
+
+    // Constructores huerto
+    public Huerto(){
+
+    }
+    public Huerto(Integer idHuerto, String estado) {
+        this.idHuerto = idHuerto;
+        this.estado = estado;
+    }
+
+    // Getters y setters huerto
+
+    public Integer getIdHuerto() {
+        return idHuerto;
+    }
+
+    public void setIdHuerto(Integer idHuerto) {
+        this.idHuerto = idHuerto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
--- src/com/huerto/model/Planta.java
+++ src/com/huerto/model/Planta.java
@@ -1,4 +1,28 @@
 package com.huerto.model;
 
 public class Planta {
+    // Definimos un enum para poder especificar la especie de cada planta
+    public enum Especie{
+        TOMATE, LECHUGA, MANGO, ZANAHORIA;
+    }
+
+    // Atributo planta
+    private Especie especie;
+
+    // Constructores planta
+    public Planta(){
+
+    }
+    public Planta(Especie especie){
+        this.especie = especie;
+    }
+
+    // Getters y setters planta
+    public Especie getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(Especie especie) {
+        this.especie = especie;
+    }
 }

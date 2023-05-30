f96bc0ff294c02e8152777eecab5badd74b26d55
Sandra López <113535029+SanLopezIgle@users.noreply.github.com>
2023-05-30 09:31:09 +0200

    Modelo (#8)
    * base de las clases que usaremos en el proyecto
    
    * cambiando nuestra clase base Huerto

--- src/com/huerto/model/Huerto.java
+++ src/com/huerto/model/Huerto.java
@@ -1,4 +1,34 @@
 package com.huerto.model;
 
 public class Huerto {
+    // Atributos huerto
+    private Integer idHuerto;
+    private Planta planta;
+
+    // Constructores huerto
+    public Huerto(Integer idHuerto){
+        this.idHuerto = idHuerto;
+    }
+    public Huerto(Integer idHuerto, Planta planta) {
+        this.idHuerto = idHuerto;
+        this.planta = planta;
+    }
+
+    // Getters y setters huerto
+
+    public Integer getIdHuerto() {
+        return idHuerto;
+    }
+
+    public void setIdHuerto(Integer idHuerto) {
+        this.idHuerto = idHuerto;
+    }
+
+    public Planta getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(Planta planta) {
+        this.planta = planta;
+    }
 }
